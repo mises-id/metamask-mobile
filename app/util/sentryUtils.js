391f63fde7c87b8ec7043bd9df2e8d407e0f8f58
freshlife001 <>
2022-08-15 17:38:27 +0800

    fix: sentry dsn

--- app/util/sentryUtils.js
+++ app/util/sentryUtils.js
@@ -4,7 +4,7 @@ import { Dedupe, ExtraErrorData } from '@sentry/integrations';
 
 const METAMASK_ENVIRONMENT = process.env['METAMASK_ENVIRONMENT'] || 'local'; // eslint-disable-line dot-notation
 const SENTRY_DSN_PROD =
-  'https://ae39e4b08d464bba9fbf121c85ccfca0@sentry.io/2299799'; // metamask-mobile
+  'https://486fde2865c84c838fb89a7e8a23022c@o1162849.ingest.sentry.io/6506117'; // metamask-mobile
 /**
  * Required instrumentation for Sentry Performance to work with React Navigation
  */
