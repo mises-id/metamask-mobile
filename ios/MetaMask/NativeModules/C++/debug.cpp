7b715e7d1054d22736682ff0f9f4b3bdca6998da
freshlife001 <>
2023-06-01 11:38:27 +0800

    fix: build in xcode 14

--- ios/MetaMask/NativeModules/C++/debug.cpp
+++ ios/MetaMask/NativeModules/C++/debug.cpp
@@ -21,27 +21,27 @@
 #endif
 
 _LIBCPP_BEGIN_NAMESPACE_STD
-
-std::string __libcpp_debug_info::what() const {
-  string msg = __file_;
-  msg += ":" + to_string(__line_) + ": _LIBCPP_ASSERT '";
-  msg += __pred_;
-  msg += "' failed. ";
-  msg += __msg_;
-  return msg;
-}
-_LIBCPP_NORETURN void __libcpp_abort_debug_function(__libcpp_debug_info const& info) {
-    std::fprintf(stderr, "%s\n", info.what().c_str());
-    std::abort();
-}
-
-_LIBCPP_SAFE_STATIC __libcpp_debug_function_type
-    __libcpp_debug_function = __libcpp_abort_debug_function;
-
-bool __libcpp_set_debug_function(__libcpp_debug_function_type __func) {
-  __libcpp_debug_function = __func;
-  return true;
-}
-
+//
+//std::string __libcpp_debug_info::what() const {
+//  string msg = __file_;
+//  msg += ":" + to_string(__line_) + ": _LIBCPP_ASSERT '";
+//  msg += __pred_;
+//  msg += "' failed. ";
+//  msg += __msg_;
+//  return msg;
+//}
+//_LIBCPP_NORETURN void __libcpp_abort_debug_function(__libcpp_debug_info const& info) {
+//    std::fprintf(stderr, "%s\n", info.what().c_str());
+//    std::abort();
+//}
+//
+//_LIBCPP_SAFE_STATIC __libcpp_debug_function_type
+//    __libcpp_debug_function = __libcpp_abort_debug_function;
+//
+//bool __libcpp_set_debug_function(__libcpp_debug_function_type __func) {
+//  __libcpp_debug_function = __func;
+//  return true;
+//}
+//
 
 _LIBCPP_END_NAMESPACE_STD
